504ea887ca148c5d6ea878bb67e72b549c497a41
Dark Side <bengillitt@gmail.com>
2024-03-10 21:06:20 +0000

    Bubble Sort

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,12 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bubbleSort(int unorderedList[])
+int* bubbleSort(int unorderedArray[], int unorderedArrayLen, int* endingArrayLen)
 {
-    
+    int* finishedArray;
+    finishedArray = (int*)malloc(unorderedArrayLen * sizeof(int));
+
+    for (int i = 0; i < unorderedArrayLen; i++) {
+        finishedArray[i] = unorderedArray[i];
+    }
+
+    *endingArrayLen = unorderedArrayLen;
+    for (int j = 0; j < *endingArrayLen; j++) {
+        for (int i = 1; i < *endingArrayLen; i++) {
+            int temp;
+            if (finishedArray[i] < finishedArray[i - 1]) {
+                temp = finishedArray[i - 1];
+                finishedArray[i - 1] = finishedArray[i];
+                finishedArray[i] = temp;
+                printf("%d > %d = %d\n", finishedArray[i], finishedArray[i - 1], temp);
+            }
+        }
+    }
+
+
+    return finishedArray;
 }
 
 int main()
 {
+    int startingArray[] = {2, 8, 5, 3, 3, 4, 1};
+    int startingArrayLen = sizeof(startingArray) / sizeof(int);
+    int* endingArrayLen = (int*)malloc(sizeof(int));
+    int* endingArray = bubbleSort(startingArray, startingArrayLen, endingArrayLen);
+
+    printf("%d\n", *endingArrayLen);
+
+    for (int i = 0; i < *endingArrayLen; i++) {
+        printf("%d, ", endingArray[i]);
+    }
+
+    free(endingArray);
+    free(endingArrayLen);
+
     return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,5 +1,3 @@
-
-    // double gpa;
-    // printf("Enter your gpa: ");
-    // scanf("%lf", &gpa); // If we want to get a double we have to use %lf
-    // printf("Your gpa is %f\n", gpa);
+    // for (int i = 0; i < unorderedArrayLen; i++) {
+    //     endingArray[i] = unorderedArray[i];
+    // }
